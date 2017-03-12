08fddaf0f99ed8f6465ec6dc494800ae47b1b200
SAMPLE <SAMPLE>
2017-03-12 08:15:17 +0100

    [rust] final version of tokio infinite reconnection loop sample

--- rust/reconnection/src/main.rs
+++ rust/reconnection/src/main.rs
@@ -3,60 +3,37 @@ extern crate tokio_core;
 extern crate tokio_line;
 
 use futures::future::{self, Future, Loop};
-use futures::{Stream, Sink};
-use futures::sync::mpsc::{self, UnboundedSender, UnboundedReceiver};
+use futures::{Stream};
+use std::{io, str};
 use tokio_core::io::{Io};
 use tokio_core::net::{TcpStream};
 use tokio_core::reactor::{Core, Handle};
 use tokio_line::LineCodec;
-use std::{io, str};
 use std::{thread, time};
 
-
-fn send_data_to_remote_server(handle: &Handle, bufrx: UnboundedReceiver<String>) -> Box<Future<Item = UnboundedReceiver<String>, Error = io::Error>> {
+fn get_connection(handle: &Handle) -> Box<Future<Item = (), Error = io::Error>> {
     let remote_addr = "127.0.0.1:9876".parse().unwrap();
     let tcp = TcpStream::connect(&remote_addr, handle);
 
     let client = tcp.and_then(move |stream| {
-        let (sender, receiver) = stream.framed(LineCodec).split();
+        let (_sender, receiver) = stream.framed(LineCodec).split();
         let reader = receiver
             .for_each(|message| {
                 println!("{}", message);
                 Ok(())
             })
-            .and_then(|bufrx| {
+            .and_then(|_| {
                 println!("CLIENT DISCONNECTED");
-                /***************** START FIXME ************/
-                let (_, bufrx) = mpsc::unbounded(); //FIXME: `bufrx` received in a clousure is actually an `()` so I can't use it, I put this here only as sample and to satisfy the compiler
-                /***************** END FIXME ************/
-                Ok(bufrx)
+                Ok(())
             });
-
-        let writer = bufrx
-            .map_err(|_| io::Error::new(io::ErrorKind::Other, "error kind returned should be the same of `sender` sink in `forward()`"))
-            .forward(sender)
-            .and_then(|(bufrx, sender)| {
-                // bufrx
-                /***************** START FIXME ************/
-                let (_, bufrx) = mpsc::unbounded(); //FIXME: `bufrx` received in a clousure is actually an `()` so I can't use it, I put this here only as sample and to satisfy the compiler
-                /***************** END FIXME ************/
-                Ok(bufrx)
-            });
-
-        reader.select(writer)
-            .map(|(bufrx, nf)| {
-                bufrx
-            })
-            .map_err(|(err, nf)| {
-                err
-            })
-
-        // let (_, bufrx) = mpsc::unbounded();
-        // reader.map(|_| bufrx).select(writer.map(|_| bufrx))
-    }).or_else(|_| {
-        println!("connection refuse");
-        Err(io::Error::new(io::ErrorKind::Other, "connection refuse"))
+        reader
     });
+
+    let client = client
+        .or_else(|_| {
+            println!("connection refuse");
+            Err(io::Error::new(io::ErrorKind::Other, "connection refuse"))
+        });
 
     Box::new(client)
 }
@@ -64,25 +41,14 @@ fn send_data_to_remote_server(handle: &Handle, bufrx: UnboundedReceiver<String>)
 fn main() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
-
-    let (buftx, bufrx) = mpsc::unbounded();
-
-    let client = future::loop_fn(bufrx, |bufrx| {
-        send_data_to_remote_server(&handle, bufrx)
-            .map(|bufrx| -> Loop<UnboundedReceiver<String>, UnboundedReceiver<String>> {
-                /***************** START FIXME ************/
-                // actually the `bufrx` received in this clouser is not the original one, but I
-                // think that the fix should be made in the `send_data_to_remote_server()` method
-                /***************** END FIXME ************/
-                Loop::Continue(bufrx)
+    let client = future::loop_fn((), |_| {
+        get_connection(&handle)
+            .map(|_| -> Loop<(), ()> {
+                Loop::Continue(())
             })
-            .or_else(|err| -> Result<Loop<UnboundedReceiver<String>, UnboundedReceiver<String>>, ()> {
-                thread::sleep(time::Duration::from_millis(50));
-                /***************** START FIXME ************/
-                let (_, bufrx) = mpsc::unbounded(); //FIXME: argument received in a clousure is actually an `io::Error` and I lost the ownership of the original bufrx so I can't use it, I put this here only as sample and to satisfy the compiler
-                /***************** END FIXME ************/
-
-                Ok(Loop::Continue(bufrx))
+            .or_else(|_| -> Result<Loop<(), ()>, ()> {
+                thread::sleep(time::Duration::from_millis(100));
+                Ok(Loop::Continue(()))
             })
     });
 
