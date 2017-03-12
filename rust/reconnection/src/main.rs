97b25b83ccc8d084fdd630424fc8037925175388
SAMPLE <SAMPLE>
2017-03-12 08:15:17 +0100

    [rust] final version of tokio infinite reconnection loop sample

--- rust/reconnection/src/main.rs
+++ rust/reconnection/src/main.rs
@@ -11,30 +11,28 @@ use tokio_core::reactor::{Core, Handle};
 use tokio_line::LineCodec;
 use std::{thread, time};
 
-
 fn get_connection(handle: &Handle) -> Box<Future<Item = (), Error = io::Error>> {
     let remote_addr = "127.0.0.1:9876".parse().unwrap();
     let tcp = TcpStream::connect(&remote_addr, handle);
 
     let client = tcp.and_then(move |stream| {
-        let (_, receiver) = stream.framed(LineCodec).split();
-        let reader = receiver.for_each(|message| {
-            println!("{}", message);
-            Ok(())
-        });
-
-        reader.and_then(|_| {
-            println!("CLIENT DISCONNECTED");
-            Ok(())
-        })
+        let (_sender, receiver) = stream.framed(LineCodec).split();
+        let reader = receiver
+            .for_each(|message| {
+                println!("{}", message);
+                Ok(())
+            })
+            .and_then(|_| {
+                println!("CLIENT DISCONNECTED");
+                Ok(())
+            });
+        reader
     });
 
     let client = client
         .or_else(|_| {
             println!("connection refuse");
-            thread::sleep(time::Duration::from_millis(100));
-            Ok(())
-            // Err(io::Error::new(io::ErrorKind::Other, "connection refuse"))
+            Err(io::Error::new(io::ErrorKind::Other, "connection refuse"))
         });
 
     Box::new(client)
@@ -44,10 +42,14 @@ fn main() {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
     let client = future::loop_fn((), |_| {
-        // Run the get_connection function and loop again regardless of its result
-        get_connection(&handle).map(|_| -> Loop<(), ()> {
-            Loop::Continue(())
-        })
+        get_connection(&handle)
+            .map(|_| -> Loop<(), ()> {
+                Loop::Continue(())
+            })
+            .or_else(|_| -> Result<Loop<(), ()>, ()> {
+                thread::sleep(time::Duration::from_millis(100));
+                Ok(Loop::Continue(()))
+            })
     });
 
     core.run(client).unwrap();
